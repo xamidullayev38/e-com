5cd1dd887c4403cc0ecbddf10f6f70105e8736ce
xamidullayev38 <xamidullayevsaidkamol@gmail.com>
2025-09-06 12:42:01 +0500

    start

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './assets/style/main.css'
+import App from './App.vue'
+
+//  normalize css cross browser uchun 
+import 'normalize.css'
+
+createApp(App).mount('#app')
